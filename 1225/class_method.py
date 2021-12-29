9b9a5285cc304738c4a3203bb8d85035e4a478fb
MENG-JIN <ssp850710@gmail.com>
2021-12-30 00:40:47 +0800

    Update class_method.py

--- 1225/class_method.py
+++ 1225/class_method.py
@@ -1,27 +1,44 @@
 import os
+import hashlib
 
-class Test():
-    offset = 0
+class Tail():
+    _offset = 0
+    _last_hast = ""
+
+    def __init__(self,parse):
+        Tail.parsefile = parse
 
     @classmethod
     def get_last_line(cls):
-        #filepath = self.parsefile
-        filepath = "/var/log/atftpd.log"
+        cls.check_md()
+        filepath = cls.parsefile
+        #filepath = "/var/log/atftpd.log"
         #global already_print_num
         data_set = []
         if not os.path.exists(filepath):
             print(f"no such file {filepath}")
             sys.exit()
             return
-        readfile = open(filepath, "r")
-        lines = readfile.readlines()
-        print("**"*20)
-        print("file size: ",readfile.tell(),"offset: ",cls.offset)
-        cls.offset = readfile.tell()
-        print("**"*20)
-        readfile.close()
+        #readfile = open(filepath, "r")
+        with open(filepath, 'r', encoding="utf-8") as readfile:
+            readfile.seek(cls._offset,0)
+            data_set = readfile.readlines()
+            print("**"*20)
+            print("file size: ",readfile.tell(),"offset: ",cls._offset, "data: ",data_set)
+            cls._offset = readfile.tell()
+        #readfile.close()
         return data_set
-
+    
+    @classmethod
+    def check_md(cls):
+        hash1 = hashlib.md5()
+        with open(cls.parsefile, 'r', encoding="utf-8") as readfile:
+            readfile.seek(500,0)
+            chk = readfile.read()
+            hash1.update(chk.encode('UTF-8'))
+            hash1 = hash1.hexdigest()
+            print("md5: ", hash1, chk)
+        return hash1
 
 #Test.get_last_line()
 #Test.get_last_line()
